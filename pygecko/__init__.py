2709af96ab48c11bdb862a713451937415cb9245
walchko <kevin.walchko@outlook.com>
2016-10-09 14:04:55 -0600

    travis fix

--- pygecko/__init__.py
+++ pygecko/__init__.py
@@ -8,9 +8,9 @@
 # from pygecko.lib import *
 # from pygecko.tools import *
 # from pygecko.servers import *
-from pkg_resources import get_distribution
+# from pkg_resources import get_distribution
 
-__version__ = get_distribution('pygecko').version
+# __version__ = get_distribution('pygecko').version
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2010 Kevin Walchko'
 __author__ = 'Kevin J. Walchko'
