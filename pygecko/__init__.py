2a579a434368229094fd672f36a6c0c1d4d3c093
Kevin <kevin.walchko@outlook.com>
2017-01-01 10:05:43 -0700

    update

--- pygecko/__init__.py
+++ pygecko/__init__.py
@@ -1,17 +1,6 @@
 #!/usr/bin/env python
 
-# from __future__ import absolute_import
-# from __future__ import print_function, division
-# from pygecko import lib
-# from pygecko import servers
-# from pygecko import tools
-# from pygecko.lib import *
-# from pygecko.tools import *
-# from pygecko.servers import *
-# from pkg_resources import get_distribution
-
-# __version__ = get_distribution('pygecko').version
-__version__ = '0.5.0'
+__version__ = '0.5.1'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2010 Kevin Walchko'
 __author__ = 'Kevin J. Walchko'
