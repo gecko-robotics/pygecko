c733a2dc6099294d6662551df85dc6553cd331ac
walchko <walchko@users.noreply.github.com>
2018-07-28 17:50:59 -0600

    added topic and keyboard

---
+++ bin/geckotopic.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+
+#
+#
+# copyright Kevin Walchko
+#
+# Basically a rostopic
+
+from __future__ import print_function
+import argparse
+import time
+# from pygecko import TopicSub
+from pygecko.transport import zmqTCP, GeckoCore
+from pygecko.multiprocessing import GeckoPy
+from pygecko.test import GeckoSimpleProcess
+
+try:
+    import simplejson as json
+except ImportError:
+    import json
+
+
+def handleArgs():
+    parser = argparse.ArgumentParser(description="""
+    A simple zero MQ message tool. It will subscribe to a topic and print the messages.
+
+    Format:
+        topic_echo host port topic
+
+    geckotopic mode[0] mode[1] [options]
+    geckotopic pub <topic-name> -m [data...]
+    geckotopic echo <topic-name>
+
+    Examples:
+        geckotopic echo hello
+        geckotopic pub hello "{'a': 3.14, 'k': [1,2,3,4]}"
+    """)
+    parser.add_argument('-m', '--msg', help='data to publish', default=None)
+    parser.add_argument('-r', '--rate', help='publish rate in hertz, default 10 hz', default=10)
+	# parser.add_argument('-o', '--once', help='publish a message once and exit')
+    parser.add_argument('mode', nargs=2, help="run geckotopic as pub (publisher of topic), echo (subscribed to topic)", default=None)
+    # parser.add_argument('-i', '--info', nargs=2, help='subscribe to messages on host port: ex. 1.2.3.4 9000', default=None)
+    # parser.add_argument('-v', '--verbose', help='display info to screen', action='store_true')
+    args = vars(parser.parse_args())
+    return args
+
+
+def publisher(**kwargs):
+    geckopy = GeckoPy()
+
+    p = geckopy.Publisher()
+
+    hertz = kwargs.get('rate', 10)
+    rate = geckopy.Rate(hertz)
+
+    topic = kwargs.get('topic')
+    msg = kwargs.get('msg')
+
+    cnt = 0
+    start = time.time()
+    while not geckopy.is_shutdown():
+        p.pub(topic, msg)  # topic msg
+        if cnt % hertz == 0:
+            print(">> {}[{:.1f}]: published {} msgs".format(topic, time.time()-start, hertz))
+        cnt += 1
+        rate.sleep()
+
+
+def subscriber(**kwargs):
+    geckopy = GeckoPy()
+
+    def f(topic, msg):
+        print(">> {}: {}".format(topic, msg))
+
+    topic = kwargs.get('topic')
+    s = geckopy.Subscriber([topic], f)
+
+    geckopy.spin()
+
+
+if __name__ == '__main__':
+    args = handleArgs()
+
+    if args['mode'] is None or args['mode'][0] not in ['pub', 'echo', 'list']:
+        print("Error: please do geckotopic --help")  # FIXME: print help
+
+    if args['mode'] == 'list':
+        raise NotImplementedError()
+
+    args['topic'] = args['mode'][1]
+
+    if args['msg'] is not None:
+        msg = args['msg']
+        args['msg'] = json.loads(args['msg'].replace("'", '"'))
+        print(args['msg'])
+        print(type(args['msg']))
+
+    # check port > 8000
+    # check valid host?
+    # if  args['info'] is not None:
+    #     args['host_port'] = (args['info'][0], args['info'][1])
+
+
+    if args['mode'][0] == 'pub':
+        p = GeckoSimpleProcess()
+        p.start(func=publisher, name='publisher', kwargs=args)
+    elif args['mode'][0] == 'echo':
+        p = GeckoSimpleProcess()
+        p.start(func=subscriber, name='subscriber', kwargs=args)
+
+
+    while True:
+        try:
+            time.sleep(1)
+        except KeyboardInterrupt:
+            break
+
+    # shutdown the processes
+    p.join(0.1)
---
+++ bin/twist_keyboard.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python3
+
+#
+# by Kevin J. Walchko 6 Aug 2016
+#
+
+
+from __future__ import print_function
+from __future__ import division
+import argparse
+import sys
+import tty
+import termios
+# from pygecko import ZmqClass as Zmq
+from pygecko.transport import zmqTCP, GeckoCore
+from pygecko.multiprocessing import GeckoPy
+from pygecko.test import GeckoSimpleProcess
+import time
+
+######################################################
+# move to messages
+from collections import namedtuple
+from the_collector.messages import Vector
+
+Twist = namedtuple('Twist', 'linear angular')
+
+######################################################
+
+
+def limit_max(x):
+    x = 1.0 if x > 1.0 else x
+    return x
+
+
+def limit_min(x):
+    x = -1.0 if x < -1.0 else x
+    return x
+
+
+# class Keyboard(object):
+#     """
+#     Keyboard class to handle input and then
+#     publish the it via ZeroMQ.
+#
+#     Still needs lots of work and a true purpose :)
+#     """
+#     def __init__(self, host, port):
+#         self.addr = (host, port)
+#         # pass
+#
+#     def run(self):
+#         pub = Zmq.Pub(self.addr, hwm=100)
+#         twist = Twist()
+#
+#         # fd = sys.stdin.fileno()
+#         # tty.setraw(sys.stdin.fileno())
+#
+#         while True:
+#             # have to do some fancy stuff to avoid sending \n all the time
+#             fd = sys.stdin.fileno()
+#             old_settings = termios.tcgetattr(fd)
+#             try:
+#                 tty.setraw(fd)
+#                 key = sys.stdin.read(1)
+#             finally:
+#                 termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+#
+#             print('>>>', key)
+#
+#             if key == 'a':
+#                 twist.angular.z += 0.1
+#                 twist.angular.z = limit_max(twist.angular.z)
+#             elif key == 'd':
+#                 twist.angular.z -= 0.1
+#                 twist.angular.z = limit_min(twist.angular.z)
+#             elif key == 'w':
+#                 twist.linear.x += 0.1
+#                 twist.linear.x = limit_max(twist.linear.x)
+#             elif key == 'x':
+#                 twist.linear.x -= 0.1
+#                 twist.linear.x = limit_min(twist.linear.x)
+#             elif key == 's':  # stop - all 0's
+#                 twist.linear.set(0.0, 0.0, 0.0)
+#                 twist.angular.set(0.0, 0.0, 0.0)
+#             elif key == 'q':
+#                 exit()
+#
+#             pub.pub('twist_kb', twist)
+
+def publisher(**kwargs):
+    geckopy = GeckoPy()
+    rate = geckopy.Rate(10)
+
+    p = geckopy.Publisher()
+
+    # hertz = kwargs.get('rate', 10)
+    # rate = geckopy.Rate(hertz)
+
+    # topic = kwargs.get('topic')
+    # msg = kwargs.get('msg')
+
+    a = [0,0,0]
+    l = [0,0,0]
+
+    # cnt = 0
+    # start = time.time()
+    while not geckopy.is_shutdown():
+
+        # have to do some fancy stuff to avoid sending \n all the time
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(fd)
+            key = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+
+        if key not in ['a', 'd', 'w', 'x', 's', 'q']:
+            continue
+
+        print('>>>', key)
+
+        if key == 'a':
+            a[2] += 0.1
+            a[2] = limit_max(a[2])
+        elif key == 'd':
+            a[2] -= 0.1
+            a[2] = limit_min(a[2])
+        elif key == 'w':
+            l[0] += 0.1
+            l[0] = limit_max(l[0])
+        elif key == 'x':
+            l[0] -= 0.1
+            l[0] = limit_min(l[0])
+        elif key == 's':  # stop - all 0's
+            a = [0,0,0]
+            l = [0,0,0]
+        elif key == 'q':
+            break
+
+        # twist = Twist(Vector(*a), Vector(*l))
+        twist = [a,l]  # FIXME: use a real message
+        p.pub('twist_kb', twist)  # topic msg
+        rate.sleep()
+
+
+# set up and handle command line args
+def handleArgs():
+    parser = argparse.ArgumentParser(description='A simple zero MQ publisher for keyboard messages')
+    parser.add_argument('-p', '--publish', nargs=2, help='publish messages to addr:port, ex. js 10.1.1.1 9000', default=['localhost', '9000'])
+    # parser.add_argument('-v', '--verbose', help='display info to screen', action='store_true')
+    args = vars(parser.parse_args())
+    return args
+
+
+def main():
+    args = handleArgs()
+
+    # print('Twist Keyboard on {}:{}'.format(args['publish'][0], args['publish'][1]))
+
+    print('------------------------')
+    print('q - quit')
+    print('------------------------')
+    print('w - forward')
+    print('a/d - left/right')
+    print('x - reverse')
+    print('s - stop')
+    print('------------------------')
+
+    publisher(**args)
+
+    # p = GeckoSimpleProcess()
+    # p.start(func=publisher, name='twist_keyboard', kwargs=args)
+    #
+    # while True:
+    #     try:
+    #         time.sleep(1)
+    #     except KeyboardInterrupt:
+    #         break
+    #
+    # # shutdown the processes
+    # p.join(0.1)
+
+
+if __name__ == "__main__":
+    main()
