a5672045fe368dd031c96977e40cae7ca61e08a4
walchko <walchko@users.noreply.github.com>
2018-09-10 21:43:07 -0600

    up

--- pygecko/multiprocessing/devices.py
+++ pygecko/multiprocessing/devices.py
@@ -13,7 +13,7 @@ from gecko import geckopy
 class Test(Source):
     def __init__(self):
         self.imu = IMU()
-        
+
     def run(self):
         a,g,m = self.imu.read()
         msg = IMU(a,g,m)
@@ -23,7 +23,7 @@ class Test(Source):
 
 def function(**kwargs):
     geckopy.init_node(**kwargs)
-    
+
     t = Test()
     t.spin(10)  # 10 Hz
 
@@ -34,20 +34,23 @@ class Publisher(object):
     """
 
     """
-    def __init__(self, addr=None, queue_size=5):
-        self.pub = geckopy.Publisher(addr, queue_size, False)
-        # if addr is None:  # FIXME: check if geckopy exists!!
-        #     addr = geckopy.core_inaddr
-        # self.pub.connect(addr, queue_size=queue_size)
+    queue_size=5
+    # def __init__(self):
+    #     # self.pub = geckopy.Publisher(None, self.queue_size, False)
+    #     # if addr is None:  # FIXME: check if geckopy exists!!
+    #     #     addr = geckopy.core_inaddr
+    #     # self.pub.connect(addr, queue_size=queue_size)
     def run(self):
         """
         this gets called once each time through the loop
         """
         pass
+
     def spin(self, hertz=20):
         """
         this spins until killed
         """
+        self.pub = geckopy.Publisher(None, self.queue_size, False)
         rate = geckopy.Rate(hertz)
         while not geckopy.kill:
             run()
@@ -67,17 +70,19 @@ from gecko import geckopy
 class RobotCommand(Sink):
     def __init__(self):
         self.robot = Robot()
-        
+
     def callback(self, topic, msg):
         if topic == b'cmd':
             geckopy.log("{}: {}".format(topic,msg))
             robot.command(msg)
+        elif topic == b'a':
+            print('hi')
 
 
 def function(**kwargs):
     geckopy.init_node(**kwargs)
-    
-    rc = RobotCommand()
+
+    rc = RobotCommand(topics=['a', 'cmd'])
     geckopy.spin()
 
 """
@@ -85,10 +90,10 @@ def function(**kwargs):
 
 
 class Subscriber(object):
-    def __init__(self, topics, addr):
+    def __init__(self, topics):
         # create subscriber and push into geckopy.subs loop
         # gecko
-        geckopy.Subscriber(topics, self.callback, addr, False)
+        geckopy.Subscriber(topics, self.callback, None, False)
         # conntect
         # setup self.callback
         # push to geckopy.subs
@@ -98,7 +103,7 @@ class Subscriber(object):
         """
         pass
 
-    
+
 class Sink(Subscriber):
     pass
 
@@ -107,17 +112,29 @@ class Rep(object):
     def __init__(self, addr):
         self.rep = 3
 
-
-class Filter(Source, Sink):
-    """Proxy?"""
-    def __init__(self, sub_topics, sub_addr=None, pub_addr=None, queue_len=5):
-        Source.__init__(self, pub_addr)
-        Sink.__init__(self, sub_topics, sub_addr)
-
-
-class Relay(Sink, Rep):
-    """Device?"""
-    def __init__(self, topics, rep_addr, sub_addr=None):
-        Sink.__init__(self, topics, sub_addr)
-        Rep.__init__(self, rep_addr)
-
+# this doesn't work:
+#   pub.spin()
+#   geckopy.spin()
+# both can't go at same time!!!
+# """
+# class Range(Filter):
+#     def callback(self, t, m):
+#         if t == b'scan':
+#             # do something
+#             self.pub('new-topic', m)
+#
+# r = Range('topic'):
+# r.spin()
+# """
+# class Filter(Subscriber, Publisher):
+#     """Proxy?"""
+#     def __init__(self):
+#         Source.__init__(self, pub_addr)
+#         Sink.__init__(self, sub_topics, sub_addr)
+#
+#
+# class Relay(Sink, Rep):
+#     """Device?"""
+#     def __init__(self, topics, rep_addr, sub_addr=None):
+#         Sink.__init__(self, topics, sub_addr)
+#        Rep.__init__(self, rep_addr)
