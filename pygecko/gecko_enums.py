fb756c3520c8c804fecbb0dd02113639d71c087b
Kevin <walchko@users.noreply.github.com>
2018-10-29 17:39:19 -0600

    Update gecko_enums.py

--- pygecko/gecko_enums.py
+++ pygecko/gecko_enums.py
@@ -3,6 +3,11 @@
 # Copyright (c) 2018 Kevin Walchko
 # see LICENSE for full details
 ##############################################
+# These are IntFlags, so you can compare them to ints. They
+# start with 1 and go to N.
+# ZmqType.pub == 1
+# ZmqType.sub == 2
+#
 from enum import IntFlag
 
 Status = IntFlag('Status','ok error topic_not_found core_not_found multiple_pub_error invalid_zmq_type')
