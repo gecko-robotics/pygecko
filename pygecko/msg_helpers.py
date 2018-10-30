41d948fe075cb899c56fcd6454597222c2620f57
walchko <walchko@users.noreply.github.com>
2018-10-30 11:35:48 -0600

    Update pygecko/msg_helpers.py

--- pygecko/msg_helpers.py
+++ pygecko/msg_helpers.py
@@ -3,13 +3,14 @@
 # Copyright (c) 2016 Kevin Walchko
 # see LICENSE for full details
 ##############################################
-import pickle
+# import pickle
+from pygecko.transport import Pickle
 from pygecko.messages import Image
-from pygecko.messages import Joystick
+# from pygecko.messages import Joystick
 import numpy as np
 
 
-def image2msg(img, compressed=False, handler=pickle):
+def image2msg(img, compressed=False, handler=Pickle):
     if compressed:
         # import cv2
         # import msgpack
@@ -21,23 +22,38 @@ def image2msg(img, compressed=False, handler=pickle):
     return msg
 
 
-def msg2image(msg):
+def msg2image(msg, handler=Pickle):
+    raw = msg.bytes
     if msg.compressed:
-        # import cv2
-        # import msgpack
-        # raw = nparr = np.fromstring(msg.bytes, np.uint8)
-        # img = np.frombuffer(raw, dtype=np.uint8)
-        # if len(msg.shape) == 3:
-        #     img = cv2.imdecode(img, cv2.IMREAD_COLOR)  # cv2.IMREAD_COLOR = 1
-        # else:
-        #     cv2.imdecode(img, cv2.IMREAD_GRAYSCALE)  # cv2.IMREAD_GRAYSCALE = 0
         raw = handler.loads(msg.bytes)
-        img = np.frombuffer(raw, dtype=np.uint8)
-        img = img.reshape(msg.shape)
-    else:
-        img = np.frombuffer(msg.bytes, dtype=np.uint8)
-        img = img.reshape(msg.shape)
+    img = np.frombuffer(raw, dtype=np.uint8)
+    img = img.reshape(msg.shape)
     return img
+
+
+
+# def msg2image(msg):
+#     raw = msg.bytes
+#     if msg.compressed:
+#         # import cv2
+#         # import msgpack
+#         # raw = nparr = np.fromstring(msg.bytes, np.uint8)
+#         # img = np.frombuffer(raw, dtype=np.uint8)
+#         # if len(msg.shape) == 3:
+#         #     img = cv2.imdecode(img, cv2.IMREAD_COLOR)  # cv2.IMREAD_COLOR = 1
+#         # else:
+#         #     cv2.imdecode(img, cv2.IMREAD_GRAYSCALE)  # cv2.IMREAD_GRAYSCALE = 0
+#         raw = handler.loads(msg.bytes)
+#     #     img = np.frombuffer(raw, dtype=np.uint8)
+#     #     img = img.reshape(msg.shape)
+#     # else:
+#     #     img = np.frombuffer(msg.bytes, dtype=np.uint8)
+#     #     img = img.reshape(msg.shape)
+#
+#     img = np.frombuffer(raw, dtype=np.uint8)
+#     img = img.reshape(msg.shape)
+#     return img
+
 
 
 # def msg2ps4(msg):
