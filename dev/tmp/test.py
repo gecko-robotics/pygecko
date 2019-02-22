b10160fe7445b91a2fbafc5dcce52e804e08dadd
walchko <walchko@users.noreply.github.com>
2019-02-21 21:06:22 -0500

    up

--- dev/tmp/test.py
+++ dev/tmp/test.py
@@ -13,6 +13,43 @@ class vec_t(namedtuple('vec_t', 'x y z')):
         cls.id = 0
         return cls.__bases__[0].__new__(cls,x,y,z)
 
+class quaternion_t(namedtuple('quaternion_t', 'w x y z')):
+    __slots__ = ()
+
+    def __new__(cls, w,x,y,z):
+        cls.id = 1
+        return cls.__bases__[0].__new__(cls,w,x,y,z)
+
+
+class wrench_t(namedtuple('wrench_t', 'force torque')):
+    __slots__ = ()
+
+    def __new__(cls, f,t):
+        cls.id = 2
+        return cls.__bases__[0].__new__(cls,f,t)
+
+class pose_t(namedtuple('pose_t', 'position orientation')):
+    __slots__ = ()
+
+    def __new__(cls, p,o):
+        cls.id = 3
+        return cls.__bases__[0].__new__(cls,p,o)
+
+class joystick_t(namedtuple('joystick_t', 'axes buttons type')):
+    __slots__ = ()
+
+    def __new__(cls, a,b,t):
+        cls.id = 4
+        return cls.__bases__[0].__new__(cls,a,b,t)
+
+
+class twist_t(namedtuple('twist_t', 'linear angular')):
+    __slots__ = ()
+
+    def __new__(cls, l,a):
+        cls.id = 5
+        return cls.__bases__[0].__new__(cls,l,a)
+
 class imu_st(namedtuple('imu_st', 'linear_accel angular_vel magnetic_field timestamp')):
     """
     Inertial measurement unit
@@ -20,69 +57,60 @@ class imu_st(namedtuple('imu_st', 'linear_accel angular_vel magnetic_field times
     __slots__ = ()
 
     def __new__(cls, a, g, m, ts=None):
-        cls.id = 2
+        cls.id = 10
         if ts:
             return cls.__bases__[0].__new__(cls, a, g, m, ts)
         else:
             return cls.__bases__[0].__new__(cls, a, g, m, time.time())
 
-# def ext_pack(x):
-#     try:
-#         return msgpack.ExtType(1, msgpack.packb([x.id,] + list(x[:]), default=ext_pack, strict_types=True))
-#     except:
-#         return x
-#
-# def ext_unpack(code, data):
-#     msgs = {
-#         0: 'vec_t',
-#         2: 'imu_st'
-#     }
-#     if code == 1:
-#         # you call this again to unpack and ext_hook for nested
-#         d = msgpack.unpackb(data, ext_hook=ext_unpack, raw=False)
-#
-#         # print d[0]   # holds class name
-#         # print d[1:]  # holds data inorder
-#         # finds constructor in namespace and calls it
-#         return globals()[msgs[d[0]]](*d[1:])
-#     return msgpack.ExtType(code, data)
-
-
 ###########################################
 
-def ext_pack2(x):
-    try:
-        return msgpack.ExtType(x.id, msgpack.packb(list(x[:]), default=ext_pack2, strict_types=True))
-    except:
-        return x
+class MsgPacker(object):
+    msgs = {
+        0: vec_t,
+        1: quaternion_t,
+        2: wrench_t,
+        3: pose_t,
+        4: joystick_t,
+        5: twist_t,
+        10: imu_st
+    }
 
-msgs = {
-    0: vec_t
-}
-msgs[2] = imu_st  # add new msg class
+    def ext_pack2(self, x):
+        try:
+            return msgpack.ExtType(x.id, msgpack.packb(list(x[:]), default=self.ext_pack2, strict_types=True))
+        except:
+            return x
 
-def ext_unpack2(code, data):
-    # print(">>", code)
-    if code in msgs.keys():
-        d = msgpack.unpackb(data, ext_hook=ext_unpack2, raw=False)
-        return msgs[code](*d)
-    return msgpack.ExtType(code, data)
+    def ext_unpack2(self, code, data):
+        # print(">>", code)
+        if code in self.msgs.keys():
+            d = msgpack.unpackb(data, ext_hook=self.ext_unpack2, raw=False)
+            return self.msgs[code](*d)
+        return msgpack.ExtType(code, data)
 
+    def pack(self, data):
+        return msgpack.packb(data, use_bin_type=True, strict_types=True,default=self.ext_pack2)
+
+    def unpack(self, data):
+        return msgpack.unpackb(data, raw=False, ext_hook=self.ext_unpack2)
+
+mp = MsgPacker()
 
 v = vec_t(1,2,3)
 print(v)
 print(v.id)
 
-mv=msgpack.packb(v, use_bin_type=True, strict_types=True,default=ext_pack2)
+mv = mp.pack(v)
 print(mv)
 print("packed",len(mv))
 
 i = imu_st(v,v,vec_t(-1,2000,1e-3))
 print(i)
-mi=msgpack.packb(i, use_bin_type=True, strict_types=True,default=ext_pack2)
+mi = mp.pack(i)
 print("packed",len(mi))
 
 print(mi)
 
-ii=msgpack.unpackb(mi, raw=False, ext_hook=ext_unpack2)
+ii = mp.unpack(mi)
 print(ii)
